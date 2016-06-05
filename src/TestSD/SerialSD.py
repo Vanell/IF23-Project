e14ce39d410140001418252a62ed3ba0813e9c02
Apache3 <tj.thomas.jeantet@gmail.com>
2016-06-05 21:53:43 +0200

    added generation of .gpx and few improvements

--- src/TestSD/SerialSD.py
+++ src/TestSD/SerialSD.py
@@ -22,10 +22,11 @@ def createGPXFile(name,gpx):
 
 def writeWaypoint(gpx,name,lat,lon):
 	gpx_waypoint = gpxpy.gpx.GPXWaypoint()
-	gpx.waypoints.append(gpx_waypoint)
 	gpx_waypoint.name = name
 	gpx_waypoint.latitude=lat
 	gpx_waypoint.longitude =lon
+	gpx.waypoints.append(gpx_waypoint)
+	
 	return
 
 def writeTrackpoint(gpx_seg,lat,lon,ele,time):
@@ -52,21 +53,27 @@ while arduino.inWaiting()==0:
 print arduino.read(arduino.inWaiting())
 arduino.write('send')
 fileText =''
-
+i = 0
 while not(cont):
-	
+	if i > 100:
+		i=0
+		arduino.write('send')
 	
 	while arduino.inWaiting()>0:
 		nbBytes = arduino.inWaiting()
 		message = arduino.read(nbBytes)
 		data = message.split()
+		#print data
+		#for c in message:
+		#	if c == '\n':
+		#		print "saut de ligne"
 		if len(data)>1 and data[-1] == 'done':
 			cont = True
 			message = message[:-6]
 		print message
 		print 'end of message'
 		fileText = fileText + message
-		
+	i+=1	
 
 	sleep(0.01)
 file = open(fileName,'w')
@@ -76,7 +83,26 @@ print 'here\'s da file nigga'
 file = open(fileName,'r')
 fileContent =''
 for line in file:
-	fileContent=fileContent + line	
-file.close()
+	fileContent=fileContent + line
 print fileContent
-	
+GPXfile = initGPX()
+print "----------------SPLITED DATA--------------"
+content = fileContent.split('\n')
+for line in content:
+	wpdata = line.split()
+	size = len(wpdata)-2
+	i=0
+	wpname=''
+	while 1:
+		wpname+=wpdata[i]
+		if i==size:
+			break
+		i+=1
+	wpname = wpdata[0]
+	wplat = wpdata[-2]
+	wplon = wpdata[-1]
+	print wpdata
+	writeWaypoint(GPXfile,wpname,wplat,wplon)
+	file.close()
+#writeWaypoint(GPXfile,"ma bite",43.876845,4.641321)
+createGPXFile(fileName,GPXfile)
