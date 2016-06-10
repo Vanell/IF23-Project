28169f649de0ccc59f525d4c4ce068d3271869ae
Apache3 <tj.thomas.jeantet@gmail.com>
2016-06-10 22:54:30 +0200

    blabla

---
+++ src/pin2/pin.h
@@ -0,0 +1,27 @@
+//Pin Configuration File 
+
+//GPS Pin
+#define TX_GPS     2
+#define RX_GPS     3
+
+//LCD Pin
+#define LCD_RS     4
+#define LCD_Enable 5
+#define LCD_D4     6
+#define LCD_D5     7
+#define LCD_D6     8
+#define LCD_D7     9
+
+//SD Pin
+#define SD_SS      10
+#define SD_MOSI    11
+#define SD_MISO    12
+#define SD_SCK     13
+
+//Buttons Pin
+#define BP0        16
+#define BP1        15
+#define BPEN       17
+
+//Others
+#define pinBat       0
