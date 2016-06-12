b855465b643cf3202e26965c0b4cd0776e3f6a79
Apache3 <tj.thomas.jeantet@gmail.com>
2016-06-13 00:39:26 +0200

    added prototypes of SD functions

--- libs/pin/pin.h
+++ libs/pin/pin.h
@@ -5,18 +5,18 @@
 #define RX_GPS     3
 
 //LCD Pin
-#define LCD_RS     4
-#define LCD_Enable 5
-#define LCD_D4     6
-#define LCD_D5     7
-#define LCD_D6     8
-#define LCD_D7     9
+// #define LCD_RS     4
+// #define LCD_Enable 5
+// #define LCD_D4     6
+// #define LCD_D5     7
+// #define LCD_D6     8
+// #define LCD_D7     9
 
 //SD Pin
 #define SD_SS      10
-#define SD_MOSI    11
-#define SD_MISO    12
-#define SD_SCK     13
+//#define SD_MOSI    11
+//#define SD_MISO    12
+//#define SD_SCK     13
 
 //Buttons Pin
 #define BP0        16
@@ -25,3 +25,6 @@
 
 //Others
 #define pinBat       0
+
+void writeWP2File(String name, String wpName,float dataGPS[]);
+void readFile(String name);
