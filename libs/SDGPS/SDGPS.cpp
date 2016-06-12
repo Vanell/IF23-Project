8e989421419a6b917340a28a36ed7bfe81a7eb10
Apache3 <tj.thomas.jeantet@gmail.com>
2016-06-13 00:40:00 +0200

    changed functions

--- libs/SDGPS/SDGPS.cpp
+++ libs/SDGPS/SDGPS.cpp
@@ -2,61 +2,88 @@
 //#include <GPS.cpp>
 
 
-//SD variables
-File myFile;
-String fileName = "test.txt";
-//String command="";
-char c;
+
 
 //SD function
 void readFile(String name){
-	char filename[name.length()+1];
-  	name.toCharArray(filename, sizeof(filename));
-	myFile = SD.open(filename);
-	if(myFile){
+    char c;
+    File myFile;
+    char filename[name.length()+1];
+    name.toCharArray(filename, sizeof(filename));
+    myFile = SD.open(filename);
+    if(myFile){
 		while(myFile.available()){
-			c = ((char)myFile.read());
+      c = ((char)myFile.read());
 			Serial.print(c);
 		}
 		myFile.close();
 		Serial.print("Done");
-	}
-	else
+	  }
+	  else
 		Serial.println("Error while opening SD for reading");
+    return;
 }
 
-void writeWP2File(String name, String wpName){
-	  char filename[name.length()+1];
-  	name.toCharArray(filename, sizeof(filename));
-  	// myFile = SD.open(filename,FILE_WRITE);
-    myFile = SD.open("text.txt",FILE_WRITE);
-  	char data[20];
-  	//dtostrf(lat,1,6,data);
+
+
+
+void writeWP2File(String name, String wpName,float dataGPS[]){
+    File myFile;
+    char filename[name.length()+1];
+    name.toCharArray(filename, sizeof(filename));
+    myFile = SD.open(filename, FILE_WRITE);
+    char data[10];
     if (myFile) {
-    	myFile.print(wpName);
-    	myFile.print(" ");
-     //  //Write latitude
-    	// dtostrf(data_GPS[2],1,6,data);
-    	// myFile.print(data);
-    	// myFile.print(" ");
-     //  //write longitude
-    	// dtostrf(data_GPS[3],1,6,data);
-    	// myFile.print(data);
-     //  //write nb Satellites and dop
-    	// sprintf(data," %i %i",(int)data_GPS[0],(int)data_GPS[1]);
-    	// myFile.println(data);
-     //  //write date
-     //  sprintf(data," %i/%i/%i",(int)data_GPS[7],(int)data_GPS[6],(int)data_GPS[5]); 
-     //  myFile.println(data);
-     //  //write time
-     //  sprintf(data," %i:%i:%i",(int)data_GPS[8],(int)data_GPS[9],(int)data_GPS[10]);
-     //  myFile.println(data);
-	    myFile.close();
-  	} 
-  	else {
-    	Serial.print("Error while opening SD for writing");
-  	}
-}
+      Serial.println("wrinting in file!");
+      myFile.print(wpName);
+      myFile.print(" ");
+      //LATITUDE
+      dtostrf(dataGPS[2],1,6,data);
+      myFile.print(data);
+      myFile.print(" ");
+      //LOGITUDE
+      dtostrf(dataGPS[3],1,6,data);
+      myFile.print(data);
+      myFile.print(" ");
+      //NB SATELLITES
+      dtostrf(dataGPS[0],1,0,data);
+      myFile.print(data);
+      myFile.print(" ");
+      //HDOP
+      dtostrf(dataGPS[1],1,0,data);
+      myFile.print(data);
+      myFile.print(" ");
+      //DAY
+      dtostrf(dataGPS[7],1,0,data);
+      myFile.print(data);
+      myFile.print("/");
+      //MONTH
+      dtostrf(dataGPS[6],1,0,data);
+      myFile.print(data);
+      myFile.print("/");
+      //YEAR
+      dtostrf(dataGPS[5],1,0,data);
+      myFile.print(data);
+      myFile.print(" ");
+      //HOURS
+      dtostrf(dataGPS[8],1,0,data);
+      myFile.print(data);
+      myFile.print(":");
+      //MINUTES
+      dtostrf(dataGPS[9],1,0,data);
+      myFile.print(data);
+      myFile.print(":");
+      //SECONDS
+      dtostrf(dataGPS[10],1,0,data);
+      myFile.println(data);
+
+      myFile.close();
+    } 
+    else {
+      Serial.println("error while opening SD for writing");
+    }
+    return;
+  }
 
 //Content of dataGPS:
 // 0 NBSAT
